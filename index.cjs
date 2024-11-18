ceced2c5d04ca3abcea8975c95377a98937e30ba
nifle3 <kuper116rus.2005@gmail.com>
2024-11-18 19:10:07 +0300

    add: log to file

--- index.cjs
+++ index.cjs
@@ -1,10 +1,24 @@
 const { Telegram } = require("telegraf");
-const { HLTV } = require('hltv');
-require('dotenv').config()
+const { HLTV } = require("hltv");
+const fs = require("fs");
+const path = require("path");
+require('dotenv').config();
 
 const chatID = process.env.CHAT_ID;
 const token = process.env.BOT_TOKEN;
 const interval = process.env.INTERVAL;
+const logInFile = process.env.LOG_IN_FILE;
+
+if (logInFile) {
+    const logFilePath = path.join(__dirname, 'app.log');
+    console.log = function(message) {
+        fs.appendFile(logFilePath, message + "\n", (err) => {
+            if (err) {
+                console.error("Error appedning into file");
+            }
+        });
+    }
+}
 
 const bot = new Telegram(token);
 const hltvLink = "https://www.hltv.org";
