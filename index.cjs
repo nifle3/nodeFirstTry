aba3e352fa33b470d41b9c1c357e4705955c6572
nifle3 <kuper116rus.2005@gmail.com>
2024-11-20 21:51:59 +0300

    add: setInterval -> setTimeout

--- index.cjs
+++ index.cjs
@@ -1,18 +1,18 @@
-const { Telegram } = require("telegraf");
-const { HLTV } = require("hltv");
-const fs = require("fs");
-const path = require("path");
+var { Telegram } = require("telegraf");
+var { HLTV } = require("hltv");
+var fs = require("fs");
+var path = require("path");
 require('dotenv').config();
 
-const chatID = process.env.CHAT_ID;
-const token = process.env.BOT_TOKEN;
-const interval = process.env.INTERVAL;
-const logInFile = process.env.LOG_IN_FILE;
-const message = process.env.MESSAGE;
+var chatID = process.env.CHAT_ID;
+var token = process.env.BOT_TOKEN;
+var interval = process.env.INTERVAL;
+var logInFile = process.env.LOG_IN_FILE;
+var message = process.env.MESSAGE;
 
-const tmpFile = "./db/data.json"
-const hltvLink = "https://www.hltv.org";
-const bot = new Telegram(token);
+var tmpFile = "./db/data.json"
+var hltvLink = "https://www.hltv.org";
+var bot = new Telegram(token);
 var linkSet = undefined;
 
 
@@ -41,34 +41,36 @@ if (fs.existsSync(tmpFile)) {
 }
 
 
-var intervalId = setInterval(async () => {
-    const news = await HLTV.getNews()
-    .catch(err => console.error(err.message));;
-    const linksToNews = news.map(value => value.link);
+var mainFunc = (
+    async () => {
+        const news = await HLTV.getNews()
+        .catch(err => console.error(err.message));;
+        const linksToNews = news.map(value => value.link);
 
 
-    if (linkSet === undefined) {
-        linkSet = new Set(linksToNews);
-    } else {
-        const newLinkSet = new Set(linksToNews);
-        const newNews = newLinkSet.difference(linkSet);
-        linkSet = newLinkSet;
+        if (linkSet === undefined) {
+            linkSet = new Set(linksToNews);
+        } else {
+            const newLinkSet = new Set(linksToNews);
+            const newNews = newLinkSet.difference(linkSet);
+            linkSet = newLinkSet;
 
-        await Promise.all(newNews.map(async element => {
-            const newNewLink = hltvLink + element;
-            await bot.sendMessage(chatID, `${message} ${newNewLink}`)
-            .catch(err => console.error(err.message));
-            console.log("send");
-        }));
+            await Promise.all(newNews.map(async element => {
+                const newNewLink = hltvLink + element;
+                await bot.sendMessage(chatID, `${message} ${newNewLink}`)
+                .catch(err => console.error(err.message));
+                console.log("send");
+            }));
 
+        }
+
+        var dataToFile = Array.from(linkSet);
+        await fs.writeFile(tmpFile, JSON.stringify(dataToFile), 'utf-8', (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+        setTimeout(mainFunc, interval);
     }
+);
 
-    var dataToFile = Array.from(linkSet);
-    await fs.writeFile(tmpFile, JSON.stringify(dataToFile), 'utf-8', (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-      }); 
-}, interval);
-
-process.on("SIGTERM", clearInterval(intervalId));
-process.on("SIGINT", clearInterval(intervalId));
+mainFunc();
